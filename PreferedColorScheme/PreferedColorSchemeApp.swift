cc753eae273f89b76454dcd1da614cdf79703e1f
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-10-01 14:56:29 +0200

    Initial Commit

---
+++ PreferedColorScheme/PreferedColorSchemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreferedColorSchemeApp.swift
+//  PreferedColorScheme
+//
+//  Created by Eric on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PreferedColorSchemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
